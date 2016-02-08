471e6052e9f72a008470d2dfc3010dd13c8f055e
unknown <brad.walbeck@gmail.com>
2016-02-07 22:19:51 -0700

    Moved non-required files to lower folder. added built package for 1.0. Added broken compression file.

---
+++ Package.bat
@@ -0,0 +1,17 @@
+@ECHO OFF
+
+SET hr=%time:~0,2%
+IF %hr% lss 10 SET hr=0%hr:~1,1%
+
+REM This sets the date like this:  mm-dd-yr-hrminsecs1/100secs
+Set TODAY=%date:~4,2%-%date:~7,2%-%date:~10,4%-%hr%%time:~3,2%%time:~6,2%%time:~9,2%
+
+ECHO.
+ECHO Zipping all files in :.\DTNSForRoku\DTNSForRoku\ and moving archive to .\DTNSForRoku\packages\
+ECHO.
+compact /c /s:"C:\Users\User\Documents\DTNSForRoku\DTNSForRoku\" "C:\Users\User\Documents\DTNSForRoku\packages\DTNSForRoku%TODAY%.zip"
+ECHO.
+
+ECHO Done.
+
+PAUSE.
